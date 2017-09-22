b1e839125fa5b06f1051f362ffd4ff3d64005422
Petter A. Urkedal <paurkedal@gmail.com>
2017-09-22 08:17:00 +0200

    Use explicit let in tupled_map to ensure order.

--- functions/contrib.ml
+++ functions/contrib.ml
@@ -8,7 +8,15 @@ let tupled_map f xs =
 
   let rec dive bs = function
    | x0 :: x1 :: x2 :: x3 :: x4 :: x5 :: x6 :: x7 :: xs ->
-      dive ((f x0, f x1, f x2, f x3, f x4, f x5, f x6, f x7) :: bs) xs
+      let y0 = f x0 in
+      let y1 = f x1 in
+      let y2 = f x2 in
+      let y3 = f x3 in
+      let y4 = f x4 in
+      let y5 = f x5 in
+      let y6 = f x6 in
+      let y7 = f x7 in
+      dive ((y0, y1, y2, y3, y4, y5, y6, y7) :: bs) xs
    | xs ->
       rise (List.map f xs) bs in
 
